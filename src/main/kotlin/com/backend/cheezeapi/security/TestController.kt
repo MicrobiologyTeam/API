c4f7614f33af7f73f4bf082c370363f616aa3fb1
Poalse <mr.sen4ek@gmail.com>
2021-04-02 22:19:33 +0700

    Fix

--- src/main/kotlin/com/backend/cheezeapi/security/TestController.kt
+++ src/main/kotlin/com/backend/cheezeapi/security/TestController.kt
@@ -3,6 +3,7 @@ package com.backend.cheezeapi.security
 import com.backend.cheezeapi.security.entity.ActionType
 import com.backend.cheezeapi.security.repository.RoleRepository
 import com.backend.cheezeapi.security.repository.SystemUserRepository
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.annotation.security.RolesAllowed
@@ -13,18 +14,23 @@ class TestController(
     private val roleRepository: RoleRepository
 ) {
     @GetMapping("change_actions")
-    fun changeActions() {
-        val systemUser = systemUserRepository.getOne(1)
-        systemUser.role.actions.addAll(listOf(ActionType.Action1, ActionType.Action2))
-        systemUserRepository.save(systemUser)
-//        val role = roleRepository.getOne(systemUser.role)
+    fun changeActions(): List<ActionType> {
+//        val systemUser = systemUserRepository.getOne(1)
+//        systemUser.role.actions.addAll(listOf(ActionType.Action1, ActionType.Action2))
+//        systemUserRepository.save(systemUser)
+//
+//        val systemUser2 = systemUserRepository.getOne(2)
+//        systemUser2.role.actions.addAll(listOf(ActionType.Action2))
+//        systemUserRepository.save(systemUser2)
+
+        return enumValues<ActionType>().toList()
     }
 
-    @RolesAllowed(ActionType.Names.Action2)
+    @PreAuthorize("hasAuthority('${ActionType.Names.Action1}')")
     @GetMapping("test1")
     fun deleteUser() = "test1"
 
-    @RolesAllowed(ActionType.Names.Action2)
+    @PreAuthorize("hasAuthority('${ActionType.Names.Action2}')")
     @GetMapping("test2")
     fun test2() = "test2"
 }
--- src/main/kotlin/com/backend/cheezeapi/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/backend/cheezeapi/security/WebSecurityConfig.kt
@@ -1,20 +1,19 @@
 package com.backend.cheezeapi.security
 
-import com.backend.cheezeapi.security.entity.ActionType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import javax.sql.DataSource
 
+
 @Configuration
 @EnableGlobalMethodSecurity(
-    jsr250Enabled = true,
-    securedEnabled = true
+    prePostEnabled = true
 )
 class WebSecurityConfig(
     private val dataSource: DataSource
@@ -28,24 +27,22 @@ class WebSecurityConfig(
             .and().httpBasic()
     }
 
-//    override fun configure(auth: AuthenticationManagerBuilder) {
-//        auth.inMemoryAuthentication()
-//            .withUser("u").password(passwordEncoder().encode("p")).roles(ActionType.Names.Action1)
-//            .and().withUser("u1").password(passwordEncoder().encode("p")).roles(ActionType.Names.Action2)
-//    }
-
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.jdbcAuthentication()
             .dataSource(dataSource)
             .passwordEncoder(passwordEncoder())
             .usersByUsernameQuery("select name, password, enabled from system_user where name=?")
-            .authoritiesByUsernameQuery("select at.actions from\n" +
-                    "\tsystem_user su join role r on (su.role_id = r.id)\n" +
-                    "\tjoin action_type at on (su.role_id = at.role_id)\n" +
-                    "\twhere su.name=?")
-
+            .authoritiesByUsernameQuery(
+                "select su.name, at.actions from" +
+                        " system_user su join role r on (su.role_id = r.id)" +
+                        " join action_type at on (su.role_id = at.role_id)" +
+                        " where su.name=?"
+            )
     }
 
+//    @Bean
+//    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+
     @Bean
-    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+    fun passwordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
 }
