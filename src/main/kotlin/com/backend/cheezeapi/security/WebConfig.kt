53f875b120f3a53af456a0b0450b346d315eea13
Poalse <mr.sen4ek@gmail.com>
2021-04-09 16:31:11 +0700

    Cors fix

--- src/main/kotlin/com/backend/cheezeapi/security/WebConfig.kt
+++ src/main/kotlin/com/backend/cheezeapi/security/WebConfig.kt
@@ -1,5 +1,6 @@
 package com.backend.cheezeapi.security
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -8,7 +9,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 @EnableWebMvc
 class WebConfig : WebMvcConfigurer {
+    @Value("\${cors.allowed.host}")
+    private lateinit var allowHost: String
+
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
+        registry.addMapping("/**").allowedOrigins(allowHost)
     }
 }
